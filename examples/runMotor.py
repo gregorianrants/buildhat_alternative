87797cc91a681e5f7d825432363a78f4c9e98f8a
gregor <gregorian_rants@hotmail.com>
2024-12-08 11:50:07 +0000

    not been here for a while and it has some changes so i am commiting them

--- examples/runMotor.py
+++ examples/runMotor.py
@@ -7,11 +7,40 @@ import sys
 from sshkeyboard import listen_keyboard
 import math
 
+class StallDetector():
+   def __init__(self,PID_controller):
+        self.stalled = False
+        self.error_not_decreasing_count = 0
+        self.stalled = False
+        self.previous_speed = 0
+        self.previous_error = 0
+        self.PID_controller = PID_controller
+      
+   def abs_error_is_decreasing(self):
+        return abs(self.PID_controller.error)<abs(self.previous_error)
+   
+   def magnitude_of_speed_above_set_point(self,speed):
+        return abs(speed)>abs(self.PID_controller.set_point)
+   
+   def update(self,speed):
+        error = self.PID_controller.error
+        if(self.abs_error_is_decreasing() or self.magnitude_of_speed_above_set_point(speed)):
+            self.error_not_decreasing_count=0
+        else:
+            self.error_not_decreasing_count+=1
+        if(abs(speed)<abs(self.PID_controller.set_point/2) and self.error_not_decreasing_count>=2):
+            self.stalled = True
+        else:
+            self.stalled = False
+        self.previous_error = error
+    
+         
+
 print('dont press anything yet we will let you know when we are ready to rock.....')
 
 with (
     BuildHat() as buildhat,
-    Motor(port="C", ser=buildhat, direction=-1,output_data_rate=1) as left_motor,
+    Motor(port="C", ser=buildhat, direction=-1) as left_motor,
 ):
     try:
         
@@ -20,17 +49,29 @@ with (
         # the motor outputs data pretty quick. note the output_data_rate (its in Hz) parameter above when setting up
         # output data rate must be 100hz or bellow.
         
-        def stop(data):
-           print(data)
-           if(left_motor.isStalled()):
-               print('stalled')
-               left_motor.run(0)
+        # def stop(data):
+        #    print(data)
+        #    if(left_motor.isStalled()):
+        #        print('stalled')
+        #        left_motor.run(0)
         
+        stall_detector = StallDetector(left_motor.PIDcontroller)
         
+        def cb(motor):
+            stall_detector.update(left_motor.data["speed_deg/sec"])
+            if stall_detector.stalled:
+                print('stalled')
+                left_motor.run(0)
+            pass
         
-        left_motor.run(degrees_per_second=180)
+        def just_a(motor):
+            print(motor.data)
+        
+        left_motor.run(degrees_per_second=250)
        
-        left_motor.add_listener(stop)
+        left_motor.add_listener(just_a,100,5)
+        time.sleep(1)
+        left_motor.add_listener(cb,10,0)
         time.sleep(10)
             
         left_motor.run(0)
--- src/buildhat_alternative/pid_controller.py
+++ src/buildhat_alternative/pid_controller.py
@@ -1,45 +1,115 @@
+# class PIDController:
+#    def __init__(self, 
+#                  proportional_constant=0, 
+#                  integral_constant=0,
+#                  derivative_constant=0,
+#                  #process_variable=0,#going to remove this
+#                  power=0):
+       
+#       self.proportional_constant = proportional_constant
+#       self.integral_constant = integral_constant
+#       self.derivative_constant = derivative_constant
+#       # Running sums
+#       self.integral_sum = 0
+#       self.previous = 0
+#       # self.process_variable = process_variable #going to remove this
+#       self.power = power
+#       self.set_point = 0
+      
+#       ##stall detection
+#       self.error_not_decreasing_count = 0
+#       self.stalled = False
+#       self.previous_speed = 0
+        
+#    def handle_proportional(self,error):
+#       return self.proportional_constant * error
+   
+#    ##this is a utility used to adjust the pid values during a refactor
+#    ## it should get removed once we check we have the math correct
+#    ##basically we have converted the process_variable from mm_sec to deg per sec
+#    # so we need to do the inverse to the pid values to make the math work.
+#    #be carefull there is some ambiguity in whether the wheel diameter is 276.401 or 276
+#    def deg_per_second_to_mm_per_second(self,deg_per_sec):
+#       return (1/360)*276*deg_per_sec
+
+  
+#    def handle_integral(self,error):
+#       self.integral_sum += error
+#       return self.integral_constant * error
+  
+#    def handle_derivative(self,error):
+#       derivative = self.derivative_constant*(error - self.previous)
+#       self.previous = error
+#       return derivative
+
+#    def get_value(self,error):
+#       p=self.handle_proportional(error)
+#       i=self.handle_integral(error)
+#       d= self.handle_derivative(error)
+#       return p+i+d
+   
+#    def abs_error_is_decreasing(self,current_error):
+#       return abs(current_error)<abs(self.previous)
+   
+#    def magnitude_of_speed_above_set_point(self,speed):
+#       return abs(speed)>abs(self.set_point)
+   
+#    def update(self,speed):
+#       if(self.set_point==0):
+#          #consider putting in a reset method to do this
+#          self.power = 0
+#          self.integral_sum = 0
+#          self.previous = 0
+#          return self.power
+#       error = speed-self.set_point
+#       if(self.abs_error_is_decreasing(error) or self.magnitude_of_speed_above_set_point(speed)):
+#          self.error_not_decreasing_count=0
+#       else:
+#          self.error_not_decreasing_count+=1
+#       if(abs(speed)<abs(self.set_point/2) and self.error_not_decreasing_count>5):
+#          self.stalled = True
+#       else:
+#          self.stalled = False
+#       adjustment = self.get_value(error)
+#       self.power = self.power-adjustment
+#       self.previous_speed = speed
+#       return max(0.6,self.power)
+
+   
+#    def set_point(self,set_point):
+#       self.set_point = set_point
+
 class PIDController:
    def __init__(self, 
                  proportional_constant=0, 
                  integral_constant=0,
                  derivative_constant=0,
                  #process_variable=0,#going to remove this
-                 power=0):
+                 manipulated_variable=0):
        
       self.proportional_constant = proportional_constant
       self.integral_constant = integral_constant
       self.derivative_constant = derivative_constant
       # Running sums
       self.integral_sum = 0
-      self.previous = 0
+      self.previous_error = 0
       # self.process_variable = process_variable #going to remove this
-      self.power = power
+      self.manipulated_variable = manipulated_variable
       self.set_point = 0
       
       ##stall detection
-      self.error_not_decreasing_count = 0
-      self.stalled = False
-      self.previous_speed = 0
+     
         
    def handle_proportional(self,error):
       return self.proportional_constant * error
    
-   ##this is a utility used to adjust the pid values during a refactor
-   ## it should get removed once we check we have the math correct
-   ##basically we have converted the process_variable from mm_sec to deg per sec
-   # so we need to do the inverse to the pid values to make the math work.
-   #be carefull there is some ambiguity in whether the wheel diameter is 276.401 or 276
-   def deg_per_second_to_mm_per_second(self,deg_per_sec):
-      return (1/360)*276*deg_per_sec
-
-  
    def handle_integral(self,error):
       self.integral_sum += error
       return self.integral_constant * error
   
    def handle_derivative(self,error):
-      derivative = self.derivative_constant*(error - self.previous)
-      self.previous = error
+      derivative = self.derivative_constant*(error - self.previous_error)
+      self.previous_error = error
       return derivative
 
    def get_value(self,error):
@@ -48,36 +118,31 @@ class PIDController:
       d= self.handle_derivative(error)
       return p+i+d
    
-   def abs_error_is_decreasing(self,current_error):
-      return abs(current_error)<abs(self.previous)
+  
    
-   def magnitude_of_speed_above_set_point(self,speed):
-      return abs(speed)>abs(self.set_point)
-   
-   def update(self,speed):
+   def update(self,proccess_variable):
       if(self.set_point==0):
          #consider putting in a reset method to do this
-         self.power = 0
+         self.manipulated_variable = 0
          self.integral_sum = 0
-         self.previous = 0
-         return self.power
-      error = speed-self.set_point
-      if(self.abs_error_is_decreasing(error) or self.magnitude_of_speed_above_set_point(speed)):
-         self.error_not_decreasing_count=0
-      else:
-         self.error_not_decreasing_count+=1
-      if(abs(speed)<abs(self.set_point/2) and self.error_not_decreasing_count>5):
-         self.stalled = True
-      else:
-         self.stalled = False
+         self.previous_error = 0
+         return self.manipulated_variable
+      error = proccess_variable-self.set_point
+      self.error = error
+     
       adjustment = self.get_value(error)
-      self.power = self.power-adjustment
-      self.previous_speed = speed
-      return max(0.6,self.power)
+      self.manipulated_variable = self.manipulated_variable-adjustment
+      self.previous_proccess_variable = proccess_variable
+      return self.manipulated_variable
 
    
    def set_point(self,set_point):
       self.set_point = set_point
+      
+      
+
+      
+      
       
       
    
